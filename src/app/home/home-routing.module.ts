fcfc60ef4b205d4b7ec6500c5ee2c12fc61695c2
Lakeenkousa <156088340+Lakeenkousa@users.noreply.github.com>
2024-03-02 12:43:04 +0000

    oefenapp

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,6 +6,44 @@ const routes: Routes = [
   {
     path: '',
     component: HomePage,
+    children: [
+      {
+        path:'Homepage',
+        children:[
+          {
+            path: '',
+            loadChildren: () => import('../home/home.module').then( m => m.HomePageModule),
+          }
+        ]
+      },
+      {
+        path:'Page1',
+        children:[
+          {
+            path:'',
+            loadChildren: () => import('../page1/page1.module').then( m => m.Page1PageModule),
+          }
+        ]
+      },
+      {
+        path:'Page2',
+        children:[
+          {
+            path:'',
+            loadChildren: () => import('../page2/page2.module').then(m => m.Page2PageModule),
+          }
+        ]
+      },
+      {
+        path:'Page3',
+        children:[
+          {
+            path:'',
+            loadChildren: () => import('../page3/page3.module').then( m => m.Page3PageModule),
+          }
+        ]
+      }
+    ]
   }
 ];
 
